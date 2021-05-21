081b128f3e92a8ac01b588e461b482ce78d3c5c1
Will <baldoswill@gmail.com>
2021-05-21 17:53:52 +0800

    Fixed Sidebar Empty Cart

--- src/components/Cart/Cart/Cart.js
+++ src/components/Cart/Cart/Cart.js
@@ -5,23 +5,28 @@ import {useSelector} from 'react-redux';
  
 export default function Products(props) {
 
-    const items = useSelector(state => state.cartReducer.items);
+    let items = useSelector(state => state.cartReducer.items);
+console.log(items)
+    if(items.length < 1){
+        items =   <h4 className = {classes.emptyCart}>Empty Cart</h4>;                                  
+    }else{
+        items = items.map(product => {
+            return <CartItem 
+            id = {product.id}
+            title = {product.title} 
+            size = {product.size} 
+            color = {product.color}
+            price = {product.price}
+            image = {product.image}
+            /> 
+            
+        });
+    }
+    
   
     return (
         <div className = {classes.cart}>
-            {
-                items && items.map(product => {
-                    return <CartItem 
-                    id = {product.id}
-                    title = {product.title} 
-                    size = {product.size} 
-                    color = {product.color}
-                    price = {product.price}
-                    image = {product.image}
-                    /> 
-                    
-                })
-            }                         
+            {items}                  
         </div>
     )
 }
